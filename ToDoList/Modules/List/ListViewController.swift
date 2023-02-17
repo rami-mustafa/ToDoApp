6906b504f30a5558ba4406a7c514c65271ed74f1
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-02-17 20:43:45 +0100

    prepare projeck start

---
+++ ToDoList/Modules/List/ListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ListViewController.swift
+//  ToDoList
+//
+//  Created by Ghaiath Alhereh on 17.02.23.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
--- ToDoList/SceneDelegate.swift
+++ ToDoList/SceneDelegate.swift
@@ -1,3 +1,9 @@
+//
+//  SceneDelegate.swift
+//  ToDoList
+//
+//  Created by Ghaiath Alhereh on 04.02.23.
+//
 
 import UIKit
 
@@ -10,7 +16,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let controller = ListViewController()
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = UINavigationController(rootViewController: controller)
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
