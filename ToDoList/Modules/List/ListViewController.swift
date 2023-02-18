fa6aa204a1a453850e88eed472777501ee3af77e
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-02-18 18:30:21 +0100

    create button

--- ToDoList/Modules/List/ListViewController.swift
+++ ToDoList/Modules/List/ListViewController.swift
@@ -9,8 +9,36 @@ import UIKit
 
 class ListViewController: UIViewController {
     
+    private lazy var newTodoButton: UIButton = {
+        var configuration = UIButton.Configuration.filled()
+        configuration.image = UIImage(systemName: "plus")
+        configuration.cornerStyle = .capsule
+        configuration.buttonSize = .large
+        let button = UIButton(configuration: configuration, primaryAction: addNewTodo())
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        configUI()
+    }
+    
+    
+    private func configUI() {
+        view.backgroundColor = .white
+        title = "Todo List"
+        guard let nav = navigationController else {return}
+        nav.view.addSubview(newTodoButton)
+        newTodoButton.rightAnchor.constraint(equalTo: nav.view.rightAnchor, constant: -20).isActive = true
+        newTodoButton.bottomAnchor.constraint(equalTo: nav.view.bottomAnchor, constant: -20).isActive = true
+        
+    }
+    
+    private func addNewTodo() -> UIAction {
+        let action = UIAction { _ in
+            print("selam")
+        }
+        return action
     }
 }
