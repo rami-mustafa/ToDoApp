c637e0e44f536eff69b8955e0a1508ed8b91834b
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-02-25 22:11:21 +0100

    core data laying the foundation

--- ToDoList/Storage/CoreData/DataManager+List.swift
+++ ToDoList/Storage/CoreData/DataManager+List.swift
@@ -2,9 +2,69 @@
  import CoreData
 
 protocol DataManagerCRUD {
-    func savaTodo()
+    func savaTodo(description: String)
+    func allTodo() -> [Todo]
+    func deleteTodo(uuid: String)
+    func updateTodo(uuid: String , title: String)
 }
 
-extension DataManager {
+extension DataManager: DataManagerCRUD{
+    func savaTodo(description: String) {
+        let todo = Todo(context: context)
+        todo.title = description
+        todo.id = UUID()
+        do{
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func allTodo() -> [Todo] {
+        let fetchListRequest: NSFetchRequest<Todo> = Todo.fetchRequest()
+        var lists = [Todo]()
+        do{
+           lists =  try context.fetch(fetchListRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+        return lists
+    }
+    
+    func deleteTodo(uuid: String) {
+        searchTodo(uuid: uuid) { todo in
+            do {
+                self.context.delete(todo)
+                try self.context.save()
+            }  catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+    }
+    
+    func updateTodo(uuid: String, title: String) {
+        searchTodo(uuid: uuid) { todo in
+            do {
+                todo.title = title
+                try self.context.save()
+            }  catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func searchTodo(uuid: String, completion: @escaping(Todo) -> Void ) {
+        let fetchListRequest: NSFetchRequest<Todo> = Todo.fetchRequest()
+        let predicate: NSPredicate = NSPredicate(format: "id == %@", uuid)
+        fetchListRequest.predicate = predicate
+        do {
+            let objects = try context.fetch(fetchListRequest)
+            guard let todo = objects.first else { return }
+            completion(todo)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
 
 }
