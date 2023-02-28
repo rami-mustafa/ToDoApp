b8106301a46923f4a97522259f1d9eaba5e57d56
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-02-28 17:23:24 +0100

    Class view value

---
+++ ToDoList/Utils/ViewValues.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+enum ViewValues {
+    static let defaultPadding: CGFloat =  20
+    static let zero =  0
+    static let defaultSizeText: CGFloat =  20
+    static let defaultRadius: CGFloat =  15
+    static let topPadding: CGFloat =  10
+    static let defaultHeight: CGFloat =  60
+
+
+}
