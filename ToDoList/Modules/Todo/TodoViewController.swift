db58f26721e28a3435c74920c82592fda9cb9df1
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-03-01 07:46:07 +0100

    renaming values

--- ToDoList/Modules/Todo/TodoViewController.swift
+++ ToDoList/Modules/Todo/TodoViewController.swift
@@ -9,22 +9,26 @@ final class TodoViewController: UIViewController {
        let textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.backgroundColor = .tertiarySystemBackground
-        textField.layer.cornerRadius = 15
-        textField.leftView = UIView(frame: .init(x: 0, y: 0, width: 0, height: 0))
+        textField.layer.cornerRadius = ViewValues.defaultRadius
+        textField.leftView = UIView(frame: .init(
+            x: ViewValues.zero,
+            y: ViewValues.zero,
+            width: Int(ViewValues.defaultPadding),
+            height: ViewValues.zero))
         textField.leftViewMode = .always
         textField.placeholder = "buraya yaz"
-        textField.font = UIFont.boldSystemFont(ofSize: 20)
+        textField.font = UIFont.boldSystemFont(ofSize: ViewValues.defaultSizeText)
         textField.returnKeyType = .done
         return textField
     }()
     
-    private let savaEditButton: UIButton = {
+    private lazy var savaEditButton: UIButton = {
         var configuration = UIButton.Configuration.tinted()
         configuration.cornerStyle = .large
         var container = AttributeContainer()
-        container.font = UIFont.boldSystemFont(ofSize: 20)
+        container.font = UIFont.boldSystemFont(ofSize: ViewValues.defaultSizeText)
         configuration.attributedTitle = AttributedString("Save" , attributes:  container)
-        let button = UIButton(configuration: configuration, primaryAction: nil)
+        let button = UIButton(configuration: configuration, primaryAction: saveTodoAction())
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -41,27 +45,38 @@ final class TodoViewController: UIViewController {
         title = "yeni not"
         
         view.addSubview(descriptionTask)
-        descriptionTask.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-        descriptionTask.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        descriptionTask.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
-        descriptionTask.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        descriptionTask.leftAnchor.constraint(equalTo: view.leftAnchor, constant: ViewValues.defaultPadding).isActive = true
+        descriptionTask.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -ViewValues.defaultPadding).isActive = true
+        descriptionTask.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: ViewValues.topPadding).isActive = true
+        descriptionTask.heightAnchor.constraint(equalToConstant: ViewValues.defaultHeight).isActive = true
         
         view.addSubview(savaEditButton)
-        savaEditButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
-        savaEditButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
-        savaEditButton.topAnchor.constraint(equalTo: descriptionTask.bottomAnchor, constant: 20).isActive = true
-        savaEditButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        savaEditButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: ViewValues.defaultPadding).isActive = true
+        savaEditButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -ViewValues.defaultPadding).isActive = true
+        savaEditButton.topAnchor.constraint(equalTo: descriptionTask.bottomAnchor, constant: ViewValues.defaultPadding).isActive = true
+        savaEditButton.heightAnchor.constraint(equalToConstant: ViewValues.defaultHeight).isActive = true
         descriptionTask.becomeFirstResponder()
         descriptionTask.delegate = self
  
     }
+    
+    func saveTodoAction() -> UIAction {
+        let action = UIAction { [weak self] _ in
+            guard let self = self else { return  }
+            self.saveTodo()
+        }
+        return action
+    
+    }
+    private func saveTodo(){
+        guard let safeText = descriptionTask.text , safeText != "" else {return}
+        newTodo.send(safeText)
+        dismiss(animated: true, completion: nil)    }
 }
 
 extension TodoViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        guard let safeText = descriptionTask.text , safeText != "" else {return true}
-        newTodo.send(safeText)
-        dismiss(animated: true, completion: nil)
+        self.saveTodo()
         return true
     }
 }
